5ee90cd63b4f0774737c0d3da06643a8838144bb
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-03-19 11:25:06 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+import Header from "./Header";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <Header />
+      <InputArea onAdd={addItem} />
+      <div className="todo-list">
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
